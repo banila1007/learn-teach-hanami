478a4ac490fbed6e6b1446acca5bf249dc438f14
martina1007 <martina1007@naver.com>
2017-11-09 20:28:58 +0900

    Update index.rb

--- apps/web/controllers/entries/index.rb
+++ apps/web/controllers/entries/index.rb
@@ -17,6 +17,7 @@ module Web::Controllers::Entries
          @entries = EntryRepository.new.most_recent
       else  
          @entries = EntryRepository.new.least_recent
+      end
     end
   end
 end
